10c92234d1e2f63843a723dc682036efb2f3a06e
earkpr <earkpr@foe-linux-02.leeds.ac.uk>
2018-03-07 10:43:38 +0000

    Added nco_script.sh to do some postprocessing on netCDF files
    Runs as part of convert.pro script
    
    Processing files in pwd get tar'd and copied to outdir.

---
+++ nco_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# K. Pringle.
+# Bash script to do some postprocessing to the GASSP data.
+# 1) Adds "Time in seconds" to long_name of TIME variable (needed for python processing).
+# 2) Changes TEMPORARY_FillValue to _FillValue which can't be done in IDL due to bug
+
+# run with:  bash nco_script.sh
+
+
+DIR='/nfs/a201/earkpr/DataVisualisation/GASSP/Nigel_Code/Level2/'
+
+find $DIR -name "*.nc" -print0 | while read -d $'\0' file
+do
+   echo "$file"
+   ncatted -O -a long_name,TIME,c,c,"Time in seconds" $file
+   ncrename -O -a .TEMPORARY_FillValue,_FillValue $file
+done
+
